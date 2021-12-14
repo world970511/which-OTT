41bc2d2c44b08b46c09a77579d5c22c7dd5f2b69
Kim Dong Hyeon <kdhahtks@gmail.com>
2021-12-14 21:48:33 +0900

    nav바 완성

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 import express from 'express';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import path from 'path';
 
 /* 셋팅 */
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => res.render('./layout/nav.ejs'));
 
-app.use('/signup', signUpRouter);
-app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
+app.use(express.static(__dirname + '/static'));
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
