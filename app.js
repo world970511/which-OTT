b481192f1aa0d9a1da0d051803221219f452b1e0
uibeom96 <uibeom96@naver.com>
2021-12-14 19:43:48 +0900

    폴더생성

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 import express from 'express';
+import dotenv from "dotenv";
+import signUpRouter from './routes/signup';
+
+/* 셋팅 */
+dotenv.config();
 
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => res.send('Hello World!'));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+app.use('/signup', signUpRouter);
+app.listen(process.env.PORT, () => console.log(`Example app listening on port ${process.env.PORT}!`));
