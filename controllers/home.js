bd48ee3d981164a663fc60b07b3c9a074be005bd
clsrns111 <clsrns1111@gmail.com>
2021-12-17 00:12:10 +0900

    라우터 리팩토링

--- controllers/home.js
+++
@@ -1,7 +0,0 @@
-import Post from '../models/Post.js';
-
-export const homeGet = async (req, res) => {
-  const posts = await Post.find({}).sort({ updatedAt: 'desc' }).exec();
-
-  res.status(200).json({ posts });
-};
