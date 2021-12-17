409477ed1261aee97aedb63c9064ff8f5e1b2437
uibeom96 <uibeom96@naver.com>
2021-12-17 17:17:29 +0900

    버튼에 마우스 올렸을떄 커서모양 바뀌게 수정완료

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 
 const connectDB = url => {
-  mongoose.connect(url, {});
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
   mongoose.connection.on('connected', () => {
     console.log('connected to MongoDB');
   });
