11de867fe1f900c5345c99704334202b3684cafc
aksmf1442 <aksmf1442@naver.com>
2021-12-19 00:28:11 +0900

    Revert hash password function

--- utils/hash-password.js
+++ utils/hash-password.js
@@ -1,10 +1,9 @@
 import crypto from 'crypto';
 
 const hashingPassword = password => {
-  const cipher = crypto.createCipher('aes-256-cbc', '열쇠');
-  let result = cipher.update(password, 'utf8', 'base64');
-  result += cipher.final('base64');
-  return result;
+  const hash = crypto.createHash('sha1');
+  hash.update(password);
+  return hash.digest('hex');
 };
 
 export default hashingPassword;
