77a97ee2857f6055d60dbd70cd81bd863add5fd7
clsrns111 <clsrns1111@gmail.com>
2021-12-20 21:21:41 +0900

    프로필사진 입력 시 프로필 사진이 화면에  보임

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,10 +1,15 @@
 const targetImg = document.querySelector('#target_img');
 const profileImg = document.querySelector('.img-profile');
+const file = document.getElementById('file');
+
+file.addEventListener('change', e => {
+  setThumnail(e);
+});
 
 targetImg.addEventListener('click', function (e) {
   // document.signform.target_url.value = document.getElementById('target_img').src;
   document.querySelector('#file').click();
-  console.log(form.action);
+
   document.querySelector('form').addEventListener('submit', function (e) {
     e.preventDefault();
     console.log(file);
@@ -25,3 +30,13 @@ function changeValue(obj) {
   document.querySelector('.img-profile').src = `img/${fileName}`;
 }
 */
+
+function setThumnail(e) {
+  const reader = new FileReader();
+
+  reader.onload = e => {
+    profileImg.src = e.target.result;
+  };
+  console.log('reader');
+  reader.readAsDataURL(e.target.files[0]);
+}
