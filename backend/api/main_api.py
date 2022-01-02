5333be018cb399c93649cadad509e910b316a34c
seoungho99 <c99@kakao.com>
2022-01-02 17:46:26 +0900

    Add comment & Add return JWT tokken

--- backend/api/main_api.py
+++ backend/api/main_api.py
@@ -1,11 +1,23 @@
-from flask import Blueprint, request, Response
-from models.models import *
-from werkzeug.security import generate_password_hash, check_password_hash
 import json
+
+from flask import Blueprint, request, Response
+from flask_jwt_extended import create_access_token
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
+from models.models import User
+
 # from utility import AlchemyEncoder
+
+'''
+main_api.py
+이 파일은 클라이언트와 통신하는 api를 작성하는 파일입니다.
+'''
+
 
 bp = Blueprint('main', __name__, url_prefix='/')
 
+# 테스트중 cors 문제를 해결하기 위한 임시조치
 resp = Response()
 resp.headers['Access-Control-Allow-Origin'] = '*'
 
@@ -20,13 +32,18 @@ def home():
 def register():
     try:
         if request.method == 'POST':
-            form = request.get_json()
-            user = tb_user.query.filter_by(user_id=form['user_id']).first()
-            if not user:
-                password = generate_password_hash(form['password'])
+            request_data = request.get_json()
+            user = User.query.filter_by(
+                user_id=request_data['user_id']).first()
 
-                user = tb_user(user_id=form['user_id'], password=password,
-                               nickname=form['nickname'], email=form['email'])
+            # 아이디 중복 체크
+            # 사용자가 없다면 회원가입 로직 진행,
+            # 사용자가 있다면 "이미 가입된 아이디입니다." + 400 response code
+            if user is None:
+                password = generate_password_hash(request_data['password'])
+
+                user = User(user_id=request_data['user_id'], password=password,
+                            nickname=request_data['nickname'], email=request_data['email'])
 
                 db.session.add(user)
                 db.session.commit()
@@ -50,40 +67,29 @@ def register():
 def login():
     try:
         if request.method == 'POST':
-            form = request.get_json()
-            user_id = form['user_id']
-            password = form['password']
+            request_data = request.get_json()
+            user_id = request_data['user_id']
+            password = request_data['password']
 
-            user_data = tb_user.query.filter_by(user_id=user_id).first()
+            user_data = User.query.filter_by(user_id=user_id).first()
 
-            if not user_data:
+            if user_data is None:
                 resp.status_code = 400
                 resp.set_data(json.dumps({'result': "없는 아이디입니다."}))
                 return resp
-            elif not check_password_hash(user_data.password, password):
+            elif check_password_hash(user_data.password, password) is False:
                 resp.status_code = 400
                 resp.set_data(json.dumps({'result': "비밀번호가 틀렸습니다."}))
                 return resp
             else:
                 # JWT 로그인 로직
                 resp.status_code = 200
-                resp.set_data(json.dumps({'result': "로그인 성공"}))
+                resp.set_data(json.dumps({
+                    'result': "로그인 성공",
+                    'nickname': user_data.nickname,
+                    'access_token': create_access_token(user_id)
+                }))
                 return resp
-    except Exception as e:
-        print(e)
-        resp.status_code = 500
-        resp.set_data(json.dumps({'result': "서버 에러"}))
-        return resp
-
-
-@bp.route('/logout')
-# 로그아웃
-def logout():
-    try:
-        # JWT 로그아웃 로직
-        resp.status_code = 200
-        resp.set_data(json.dumps({'result': "로그아웃 성공"}))
-        return resp
     except Exception as e:
         print(e)
         resp.status_code = 500
